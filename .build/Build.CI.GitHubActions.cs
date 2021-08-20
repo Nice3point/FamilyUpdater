31420f63ac4dfcc631f842147fe0086af9103f0e
Nice3point <nice3point@gmail.com>
2021-08-20 12:49:49 +0300

    Update GitFlow

--- .build/Build.CI.GitHubActions.cs
+++ .build/Build.CI.GitHubActions.cs
@@ -6,8 +6,8 @@
     OnPushBranches = new[] { "main" })]
 partial class Build
 {
-// - uses: actions/upload-artifact@v1
-// with:
-// name: Product
-// path: output
+//      - uses: actions/upload-artifact@v1
+//        with:
+//          name: output
+//          path: output
 }
