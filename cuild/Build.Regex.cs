2ba0aed11952bf7631773bd53dfbb177fa9ed631
Nice3point <nice3point@gmail.com>
2023-12-20 16:23:46 +0300

    Rename folder

--- cuild/Build.Regex.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-sealed partial class Build
-{
-    readonly Regex ArgumentsRegex = ArgumentsRegexGenerator();
-
-    [GeneratedRegex("'(.+?)'", RegexOptions.Compiled)]
-    private static partial Regex ArgumentsRegexGenerator();
-}
